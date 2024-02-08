440b1338dc3f4e797edbf01d09bc540fd87abc4c
AnnaMatveenko-qa <annamatveenko85@gmail.com>
2024-02-08 15:51:19 +0200

    add  homework

--- src/test/java/org/example/matveenko/ua/hw4/ActionsExampleTest.java
+++ src/test/java/org/example/matveenko/ua/hw4/ActionsExampleTest.java
@@ -6,46 +6,46 @@ import matveenko.ua.lesson2.hw4.DuoPage;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.time.Duration;
-import java.util.Objects;
+
 
 public class ActionsExampleTest {
-    private WebDriver webDriver;
+    private WebDriver driver;
 
-
+@BeforeSuite
+public void setUp(){
+    WebDriverManager.chromedriver().clearDriverCache().setup();
+    WebDriverManager.chromedriver().clearResolutionCache().setup();
+}
 
     @BeforeTest
-    public void before() {
-        WebDriverManager.chromedriver().clearDriverCache().setup();
-        WebDriverManager.chromedriver().clearResolutionCache().setup();
-        ChromeOptions options = new ChromeOptions();
-      //  options.addArguments("--no-sandbox");
-       // options.addArguments("--remote-debugging-pipe");
-        options.addArguments("--headless");
-      //  options.addArguments("--disable-dev-shm-usage");
-        webDriver = new ChromeDriver(options);
-        webDriver.get("https://dou.ua/");
-       webDriver.manage().window().setSize(new Dimension(1600, 900));
-        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+    @BeforeMethod
+    public void InitDriver() throws RuntimeException {
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        this.driver = new ChromeDriver();
+        driver.manage().window().setSize(new Dimension(1600, 900));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
+        driver.get("https://dou.ua/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 
     }
 
     @AfterTest
     public void after() {
-        webDriver.quit();
-         WebDriverManager.chromedriver().quit();
-    }
+        driver.quit();
 
+    }
+@AfterSuite
+public void tearDown(){
+    WebDriverManager.chromedriver().quit();
+}
     @Test
     public void test1() {
-        DuoPage duoPage = new DuoPage(webDriver);
+        DuoPage duoPage = new DuoPage(driver);
         String actual = duoPage.getLinkText(0);
         String expected = "Мінус 683 вакансії за місяць. Огляд IT-ринку праці, грудень 2023";
         Assert.assertNotEquals(expected, actual);
@@ -53,7 +53,7 @@ public class ActionsExampleTest {
 
     @Test
     public void test2() {
-        DuoPage duoPage = new DuoPage(webDriver);
+        DuoPage duoPage = new DuoPage(driver);
         duoPage.clickLink(5);
     }
 }
--- src/test/java/org/example/matveenko/ua/hw5/DataProviderExampleTests.java
+++ src/test/java/org/example/matveenko/ua/hw5/DataProviderExampleTests.java
@@ -3,22 +3,20 @@ package org.example.matveenko.ua.hw5;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
-import java.nio.file.Path;
+
 import java.time.Duration;
-import java.util.ArrayList;
+
 import java.util.List;
-import java.util.Objects;
+
 
 public class DataProviderExampleTests {
 
-    private WebDriver webDriver;
+    private WebDriver driver;
 
     @DataProvider(name = "Data")
     public Object[] searchData() {
@@ -29,39 +27,38 @@ public class DataProviderExampleTests {
 
 
     @BeforeMethod
-    public void before() {
+    public void InitDriver() throws RuntimeException {
         WebDriverManager.chromedriver().clearDriverCache().setup();
         WebDriverManager.chromedriver().clearResolutionCache().setup();
-      ChromeOptions options = new ChromeOptions();
-       // options.addArguments("--no-sandbox");
-        //options.addArguments("--remote-debugging-pipe");
-        options.addArguments("--headless");
-        //options.addArguments("--disable-dev-shm-usage");
-        webDriver = new ChromeDriver(options);
-        webDriver.get("https://www.work.ua/");
-        webDriver.manage().window().setSize(new Dimension(1600, 900));
-        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        this.driver = new ChromeDriver();
+        driver.manage().window().setSize(new Dimension(1600, 900));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
+        driver.get("https://www.work.ua/");
+
+
 
     }
 
     @AfterMethod
     public void after() {
-        webDriver.quit();
+        driver.quit();
         WebDriverManager.chromedriver().quit();
     }
 
     @Test(dataProvider = "Data")
     public void checkSearch(String data) {
-        WebElement input = webDriver.findElement(By.id("search"));
-        WebElement inputCity = webDriver.findElement(By.
+        WebElement input = driver.findElement(By.id("search"));
+        WebElement inputCity = driver.findElement(By.
                 xpath("//div/input[@class='js-main-region form-control ']"));
         inputCity.click();
         inputCity.clear();
         inputCity.sendKeys("Вся Украина");
         input.sendKeys(data);
         input.sendKeys(Keys.ENTER);
-        List<WebElement> links = webDriver.findElements(By.xpath("//h2[@class='cut-top cut-bottom']/a"));
-        new WebDriverWait(webDriver, Duration.ofSeconds(5)).until(ExpectedConditions.
+        List<WebElement> links = driver.findElements(By.xpath("//h2[@class='cut-top cut-bottom']/a"));
+        new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.
                 presenceOfAllElementsLocatedBy(By.xpath("//h2[@class='cut-top cut-bottom']/a")));
         Assert.assertTrue(links.get(4).getText().toLowerCase().contains(data.toLowerCase()));
     }
